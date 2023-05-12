b2710a3cae585b8fa1bd9aae881315a79500aade
hirotoshy <ubuntu@ip-172-31-26-253.ap-northeast-1.compute.internal>
2023-05-12 13:03:16 +0000

    dashboard page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,12 @@
+@if (Auth::check())
+    {{-- メッセージ作成ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('tasks.create') }}">新規タスクの投稿</a></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">ログアウト</a></li>
+@else
+    {{-- ユーザ登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">新規登録</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('login') }}">ログイン</a></li>
+@endif
